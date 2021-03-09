b838eac03812ec9095737fe2a926a7ae9ec87b12
CongL3 <congle.11@gmail.com>
2021-03-10 01:49:16 +0700

    corrects comments spacing

--- InstragramClone/ContentView.swift
+++ InstragramClone/ContentView.swift
@@ -58,13 +58,11 @@ struct ContentView: View {
 											.font(.system(size: 12))
 									}
 									
-									VStack {
-//										HStack(alignment: .lastTextBaseline, spacing: 0) {
+									VStack(alignment: .leading, spacing: 5) {
 											Text("Alice This is my picture from Hoi An")
 												.font(.system(size: 12))
 												.padding(.leading, 10)
 												
-//										}
 										HStack {
 											Text("Cong My princess!!!!! <3")
 												.font(.system(size: 12))
@@ -76,17 +74,33 @@ struct ContentView: View {
 												.frame(width: 20)
 												.padding(.trailing, 10)
 										}
+
 										HStack {
 											Text("Cong Tooo cuteeeeeeeeeeeeeeeee")
-											.font(.system(size: 12))
+												.font(.system(size: 12))
+												.padding(.leading, 10)
+											Spacer()
+											Image("heart")
+												.resizable()
+												.aspectRatio(contentMode: .fit)
+												.frame(width: 20)
+												.padding(.trailing, 10)
 										}
 										Text("view all 10 comments")
 											.font(.system(size: 12))
 											.foregroundColor(.gray)
+											.padding(.leading, 10)
 										
 										HStack {
-											Text("Cong My baby xD")
-											.font(.system(size: 12))
+											Text("Cong My Baby xD")
+												.font(.system(size: 12))
+												.padding(.leading, 10)
+											Spacer()
+											Image("heart")
+												.resizable()
+												.aspectRatio(contentMode: .fit)
+												.frame(width: 20)
+												.padding(.trailing, 10)
 										}
 									}
 								}
